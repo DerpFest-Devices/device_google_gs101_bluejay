3ddedc966c18f23d5704c9aa75b80faef86ccb6a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-10 08:37:13 +0800

    gs101: set default irqaffinity to little cluster
    Change-Id: I4ef7375910523b3ea0fbd0b41519569f79f5a781

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -45,6 +45,7 @@ BOARD_KERNEL_CMDLINE += cma_sysfs.experimental=Y
 BOARD_KERNEL_CMDLINE += rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy
 BOARD_KERNEL_CMDLINE += swiotlb=noforce
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
+BOARD_KERNEL_CMDLINE += irqaffinity=0-3
 BOARD_BOOTCONFIG += androidboot.boot_devices=14700000.ufs
 
 TARGET_NO_BOOTLOADER := true
